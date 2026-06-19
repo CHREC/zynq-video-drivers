9605344511ada94a6cfe95adc6ee9c9d8eda1e50
Ian Peitzsch <irpeitzsch@gmail.com>
2026-06-19 08:46:00 -0400

    I hate it here

--- psee-dma.c
+++ psee-dma.c
@@ -84,7 +84,7 @@ static inline u32 read_reg(struct psee_dma *dma, u32 addr)
 
 static inline void write_reg(struct psee_dma *dma, u32 addr, u32 value)
 {
-	printk(KERN_WARNING "PSEE WRITE REG %x %x\n", addr, value);
+	printk(KERN_WARNING "PSEE WRITE REG %x %x\n", dma->iomem + addr, value);
 	iowrite32(value, dma->iomem + addr);
 	printk(KERN_WARNING "PSEE WROTE REG\n");
 }
