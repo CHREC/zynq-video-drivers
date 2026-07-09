eb1c02fda306a78d6a4e43c0baee4a3499bc70e9
Ian Peitzsch <irpeitzsch@gmail.com>
2026-07-09 09:56:56 -0400

    trying standard xilinx driver

--- psee-csi2rxss.c
+++ psee-csi2rxss.c
@@ -23,7 +23,7 @@
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-fwnode.h>
 #include <media/v4l2-subdev.h>
-#include "xilinx-vip.h"
+// #include "xilinx-vip.h"
 
 /* Register register map */
 #define XCSI_CCR_OFFSET		0x00
@@ -225,6 +225,42 @@ static const u32 xcsi2dt_mbus_lut[][2] = {
  *
  * This structure contains the device driver related parameters
  */
+
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <media/media-device.h>
+#include <media/v4l2-async.h>
+#include <media/v4l2-ctrls.h>
+#include <media/v4l2-device.h>
+
+/**
+ * struct xvip_composite_device - Xilinx Video IP device structure
+ * @v4l2_dev: V4L2 device
+ * @media_dev: media device
+ * @dev: (OF) device
+ * @notifier: V4L2 asynchronous subdevs notifier
+ * @dmas: list of DMA channels at the pipeline output and input
+ * @v4l2_caps: V4L2 capabilities of the whole device (see VIDIOC_QUERYCAP)
+ * @lock: This is to ensure all dma path entities acquire same pipeline object
+ * @atomic_streamon: Indicates that multi dma media pipe will get enabled
+ *  with single dma start
+ */
+struct xvip_composite_device {
+	struct v4l2_device v4l2_dev;
+	struct media_device media_dev;
+	struct device *dev;
+
+	struct v4l2_async_notifier notifier;
+
+	struct list_head dmas;
+	u32 v4l2_caps;
+	struct mutex lock; /* lock to protect xvip pipeline instance */
+	bool atomic_streamon;
+};
+
+int xvip_graph_pipeline_start_stop(struct xvip_composite_device *xdev,
+				   struct xvip_pipeline *pipe, bool on);
+
 struct xcsi2rxss_state {
 	struct v4l2_subdev subdev;
 	struct v4l2_mbus_framefmt format;
